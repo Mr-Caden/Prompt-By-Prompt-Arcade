8fbc4d3dcafb7a0a2b443fd6d1f7019e56366be8
Caden Gamache <mrcgamache@outlook.com>
2026-03-02 19:33:09 -0700

    Update base.js

--- js/games/base.js
+++ js/games/base.js
@@ -1,21 +1,39 @@
 /**
  * Arcade Game Base Class
+ * All games and screens must extend this class to work with the Arcade Engine.
  */
+
 class ArcadeGame {
+    /**
+     * Meta information used by the engine for random selection and UI rendering.
+     */
+    static get meta() {
+        return {
+            title: "Unknown Game",
+            minPlayers: 1,
+            maxPlayers: 8
+        };
+    }
+
     constructor(p, matterEngine, players) {
         this.p = p;
         this.matter = matterEngine;
         this.players = players;
     }
+
     setup() {}
     update() {}
     draw() {}
     onInput(playerId, data) {}
-    onPlayerJoin(player) {} // NEW
-    onPlayerLeave(playerId) {} // NEW
+    onPlayerJoin(player) {}
+    onPlayerLeave(playerId) {}
+    
     cleanup() {
         Matter.World.clear(this.matter.world);
         Matter.Engine.clear(this.matter);
     }
-    getMobileUI() { return { layout: 'default' }; }
+    
+    getMobileUI() {
+        return { layout: 'default' };
+    }
 }
