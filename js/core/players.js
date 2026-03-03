22c1960834efe7b3fcf14af20b1c8b2374ecc970
Caden Gamache <mrcgamache@outlook.com>
2026-03-02 19:35:27 -0700

    Update players.js

--- js/core/players.js
+++ js/core/players.js
@@ -1,6 +1,5 @@
 /**
  * Player Manager
- * Manages the state, customization, and scores of all connected players.
  */
 
 class Player {
@@ -9,16 +8,12 @@ class Player {
         this.name = "Player";
         this.isReady = false;
         this.score = 0;
-        
-        // Customization
         this.hue = 210;
         this.faceStyle = 'classic';
         this.variant = 'boy';
         this.hat = 'none';
         this.mask = 'none';
         this.back = 'none';
-
-        // Unique random offset so players blink independently
         this.blinkOffset = Math.random() * 4000;
     }
 
@@ -30,28 +25,19 @@ class Player {
         if (data.hat !== undefined) this.hat = data.hat;
         if (data.mask !== undefined) this.mask = data.mask;
         if (data.back !== undefined) this.back = data.back;
-        if (data.isReady !== undefined) this.isReady = data.isReady;
+        if (data.isReady !== undefined) this.isReady = data.isReady; // SYNC READY STATE
     }
 
-    /**
-     * Draws the player character onto the p5 canvas.
-     * Keeps colorMode safety by utilizing push() and pop().
-     */
     draw(p, x, y, scale = 1, emotion = 'normal') {
         const time = p.millis();
         const isBlinking = ((time + this.blinkOffset) % 4000) < 150;
-
         p.push();
         p.translate(x, y);
         p.scale(scale);
-        
-        // Temporarily switch to HSB to render character
         p.colorMode(p.HSB, 360, 100, 100, 1);
         
-        // Render layers using CharLib
         if (CharLib.back[this.back]) CharLib.back[this.back](p, this.hue);
 
-        // Main Body
         p.fill(this.hue, 80, 95);
         p.stroke(0, 0, 15);
         p.strokeWeight(4);
@@ -64,42 +50,22 @@ class Player {
         if (CharLib.masks[this.mask]) CharLib.masks[this.mask](p, this.hue);
         if (CharLib.hats[this.hat]) CharLib.hats[this.hat](p, this.hue);
 
-        p.pop(); // Returns to previous color mode
+        p.pop();
     }
 }
 
 class PlayerManager {
-    constructor() {
-        this.players = new Map();
-    }
-
+    constructor() { this.players = new Map(); }
     addPlayer(id) {
-        if (!this.players.has(id)) {
-            this.players.set(id, new Player(id));
-        }
+        if (!this.players.has(id)) this.players.set(id, new Player(id));
         return this.players.get(id);
     }
-
-    removePlayer(id) {
-        this.players.delete(id);
-    }
-
-    getPlayer(id) {
-        return this.players.get(id);
-    }
-
-    getAllPlayers() {
-        return Array.from(this.players.values());
-    }
-
+    removePlayer(id) { this.players.delete(id); }
+    getPlayer(id) { return this.players.get(id); }
+    getAllPlayers() { return Array.from(this.players.values()); }
     updatePlayer(id, data) {
         const player = this.players.get(id);
-        if (player) {
-            player.updateCustomization(data);
-        }
+        if (player) player.updateCustomization(data);
     }
-
-    resetReadyStates() {
-        this.players.forEach(p => p.isReady = false);
-    }
+    resetReadyStates() { this.players.forEach(p => p.isReady = false); }
 }
